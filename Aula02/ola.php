3d0cae3abb1fe8512b144d45573606b02a187b13
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-03 21:37:07 +0100

    Aula02 - Olá Mundo em PHP 5

--- Aula02/ola.php
+++ Aula02/ola.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    	h2{
+    		color: blue;
+    		text-shadow: 1px 1px 1px black;
+    	}
+    </style>
 </head>
 <body>
 	<h1>Testando PHP</h1>
 	<?php 
-		echo "Olá Mundo";
+		echo "<h2>Olá <br> Mundo!</h2>";
 	?>
     
 </body>
