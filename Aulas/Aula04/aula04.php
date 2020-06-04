684bebe66c04a9631a24bc48cd0d5ee99d4018cb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 11:11:15 +0100

    Aula04 - Variaveis

---
+++ Aulas/Aula04/aula04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula04</title>
+</head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
