12077afb1a530e7bf08ba8541fc8b8c7f2e2eb41
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 11:41:13 +0100

    Aula05 - Operadores Aritméticos

---
+++ Aulas/Aula05/operadores.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula05</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           echo "<p>Operadores aritméticos</p><br>";
+
+           $n1 = 3;
+           $n2 = 2;
+
+           echo "A soma vale ".($n1 + $n2);
+           echo "<br>A subtração vale ".($n1 - $n2);
+           echo "<br>A Multiplicação vale ".($n1*$n2);
+           echo "<br> A divisão vale ".($n1/$n2);
+           echo "O resto da divisão é " ($n1%$n2);
+
+
+            
+        ?>
+    </div>
+</body>
+</html>
