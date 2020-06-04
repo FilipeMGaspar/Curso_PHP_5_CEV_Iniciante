d65811043ba98dbb63f8f2e04b9526ea76345526
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 11:34:37 +0100

    Aula05 - Operadores Aritméticos

--- Aulas/Aula05/aula05.php
+++ Aulas/Aula05/aula05.php
@@ -10,8 +10,15 @@
 <body>
     <div>
         <?php
-           
+           echo "<p>Operadores aritméticos</p><br>";
 
+            $n1 =3;
+            $n2 = 2;
+            $s = $n1 + $n2;
+
+            echo "A soma entre $n1 e $n2 é igual a $s<br>";
+
+            
         ?>
     </div>
 </body>
