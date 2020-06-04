cc5adb133181b6ec479a8da6e406a69a73fac13f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 11:28:22 +0100

    Aula05 - Operadores Aritméticos

---
+++ Aulas/Aula05/aula05.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula05</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           
+
+        ?>
+    </div>
+</body>
+</html>
