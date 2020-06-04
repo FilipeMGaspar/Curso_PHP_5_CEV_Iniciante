2a52f2d1a71c22ff5cf20e86fd1de7bd1fa0ec51
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 12:03:00 +0100

    Aula05 - Operadores Aritméticos. Funções

--- Aulas/Aula05/funcoesaritmeticas.php
+++ Aulas/Aula05/funcoesaritmeticas.php
@@ -10,8 +10,9 @@
     <style>
         h2{
             color: blue;
-            font-size: 13pt;
-            border-top: 0px;
+            font-size: 14pt Arial;
+            margin-top: 0px;
+            font-weight: bold;
         }
     </style>
 </head>
@@ -24,6 +25,13 @@
            $v2 = $_GET["y"];   
            
             echo "<h2>Valores recebidos: $v1 e $v2.</h2>";
+           
+            echo "<br>O valor absoluto de $v2 é ". abs($v2);            
+           
+            echo "<br> O valor de $v1<sup>$v2</sup> é ". pow($v1, $v2);
+
+            echo "<br>A raiz de $v1 é ". sqrt($v1);
+
             
         ?>
     </div>
