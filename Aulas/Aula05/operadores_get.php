d14f7f8c9946a9c1fae9e39ab656b42872ba98b9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 11:50:39 +0100

    Aula05 - Operadores Aritméticos. Método $_GET

---
+++ Aulas/Aula05/operadores_get.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula05</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           echo "<p>Operadores aritméticos</p><br>";
+
+           $n1 = $_GET["a"];
+           $n2 = $_GET["b"];
+           echo "<h2>Valores recebidos: $n1 e $n2</h2>";
+           $media = ($n1+$n2)/2;
+           echo "A soma vale ".($n1 + $n2);
+           echo "<br>A subtração vale ".($n1 - $n2);
+           echo "<br>A Multiplicação vale ".($n1 * $n2);
+           echo "<br> A divisão vale ".($n1/$n2);
+           echo "<br>O resto da divisão é ". ($n1%$n2);
+           echo "<br> A média é $media";
+            
+        ?>
+    </div>
+</body>
+</html>
