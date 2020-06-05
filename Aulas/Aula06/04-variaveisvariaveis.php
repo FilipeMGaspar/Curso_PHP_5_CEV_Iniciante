542259aa976e71d66faaa5231b3069156a6f5dff
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 12:45:07 +0100

    Aula06 - Operadores de Atribuição -Variaveis de variaveis

---
+++ Aulas/Aula06/04-variaveisvariaveis.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula06 </title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+          $x ="abc";
+          $$x = "def";//O nome da variavel é atribuido pelo valor da variavel $x
+
+          echo "O conteudo da variavel X é $x";
+          echo "A variavel criada recebeu o valor de $abc"
+        ?>
+    </div>
+</body>
+</html>
