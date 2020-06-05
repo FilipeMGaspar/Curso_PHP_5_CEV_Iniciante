f9b2ba388c9ca801d6aa728fb5f1821e516b6e89
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 12:33:19 +0100

    Aula06 - Operadores de Atribuição -Variaveis referenciadas

---
+++ Aulas/Aula06/03-referencia.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula06 </title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           $a = 3;
+           $b = $a;
+           $b + = 5;
+
+           echo "A variavel A vale $a";
+           echo "A variavel B vale $b";
+
+        ?>
+    </div>
+</body>
+</html>
