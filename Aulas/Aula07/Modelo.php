99fa0446407508105439706e0c925d7b742ec8ae
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 12:49:38 +0100

    Aula07 - Operadores Relacionais

---
+++ Aulas/Aula07/Modelo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula07</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+
+        ?>
+    </div>
+</body>
+</html>
