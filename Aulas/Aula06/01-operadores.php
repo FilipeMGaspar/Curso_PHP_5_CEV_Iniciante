88c821338b3b977d2284d4867d0a226ace63645a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 12:15:06 +0100

    Aula06 - Operadores de Atribuição

--- Aulas/Aula06/01-operadores.php
+++ Aulas/Aula06/01-operadores.php
@@ -12,10 +12,13 @@
         <?php
           echo "<h1>Operadores de atribuição</h1>";
           $preco = $_GET["p"];
+          $precod = $_GET["p"];
           echo "O preço do produto é $preco €";
           //$preco = $preco + ($preco*10/100);
           $preco += $preco*10/100; 
           echo "<br>O preço com 10% de aumento será $preco €";
+          $precod -= $precod*15/100;
+          echo "<br>O preço com 15% de desconto será de $precod €";
         ?>
     </div>
 </body>
