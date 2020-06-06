e2052e607eb15eade08652e8c506035269a040fd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-06 11:54:50 +0100

    Aula07 - Operadores Relacionais - Operador Unário -situação do aluno

---
+++ Aulas/Aula07/03-situacao.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula07</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            $nota1 = $_GET["n1"];
+            $nota2 = $_GET["n2"];
+
+            $media = ($nota1+$nota2)/2;
+
+            echo "A média entre $nota1 e $nota2 é $media<br>";
+        ?>
+    </div>
+</body>
+</html>
