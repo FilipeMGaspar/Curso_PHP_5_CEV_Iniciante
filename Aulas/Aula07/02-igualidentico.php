7d8482b45a8c9573747a944769cf254a1b5a39f4
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-06 11:48:22 +0100

    Aula07 - Operadores Relacionais - Operador Unário - Igual e Identico

---
+++ Aulas/Aula07/02-igualidentico.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula07</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+           $a = 3;
+           $b = "3";
+           
+           $r = ($a == $b) ? "SIM" : "NÃO";
+
+           echo "As variaveis A e B são Iguais: $r";
+        ?>
+    </div>
+</body>
+</html>
