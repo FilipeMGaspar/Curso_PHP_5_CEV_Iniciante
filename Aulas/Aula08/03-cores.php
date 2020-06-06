ef3f11225b305c3037226058b8f132869a72bdf2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-06 13:02:00 +0100

    Aula08 - Integração com HTML5 com PHP - Formulários - Alterarndo as CSS

--- Aulas/Aula08/03-cores.php
+++ Aulas/Aula08/03-cores.php
@@ -6,11 +6,17 @@
     , initial-scale=1.0">
     <title>Aula08 - Integrando HTML5 com PHP </title>
     <link rel="stylesheet" href="_css/estilo.css">
+
+    <?php
+        $txt = isset($_GET["t"]) ? $_GET["t"] : "Texto genérico";
+        $tam = isset($_GET["tam"]) ? $_GET["tam"] : "12pt";
+        $cor = isset($_GET["cor"]) ? $_GET["cor"] : "#000000";
+    ?>
 </head>
 <body>
     <div>
         <?php
-            
+            echo $txt;
         ?>
     </div>
 </body>
