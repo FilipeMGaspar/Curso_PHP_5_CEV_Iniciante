898281766e31d849548cb392d165b4e113c57dfe
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-08 09:10:58 +0100

    Aula09 - Estrutura Condicional - if{} else{} e elseif{}

---
+++ Aulas/Aula09/03-exercicio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula09 - estrutura Condicional if</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            
+        ?>
+    </div>
+</body>
+</html>
