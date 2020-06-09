bb12c0044219909d3c6adbdea1ba5a1e79969ea7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-09 08:24:43 +0100

    Aula09 - Estrutura Condicional - if{} else{} e elseif{}

--- Aulas/Aula09/03-exercicio.php
+++ Aulas/Aula09/03-exercicio.php
@@ -6,7 +6,12 @@
     , initial-scale=1.0">
     <title>Aula09 - estrutura Condicional if</title>
     <link rel="stylesheet" href="_css/estilo.css">
+    <?php 
+        $nota1 = $_GET["n1"];
+        $nota2 = $_GET["n2"];
+        $media = ($nota1 + $nota2)/2;
 
+    ?>
     <!-- 
         Em falta a Formatação de cores
     -->
@@ -14,9 +19,7 @@
 <body>
     <div>
         <?php
-            $nota1 = $_GET["n1"];
-            $nota2 = $_GET["n2"];
-            $media = ($nota1 + $nota2)/2;
+            
 
             echo "Obteve $nota1 valores no 1º teste <br> E $nota2 valores no 2º Teste";
             echo"<br>Com média de $media valores.";
