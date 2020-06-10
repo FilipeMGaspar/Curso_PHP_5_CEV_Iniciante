09d0212e0b5efba032593bb480c59c7645a8827d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-10 20:31:57 +0100

    Aula10 - Estrurura condicional Switch

--- Aulas/Aula10/02-exercicio.php
+++ Aulas/Aula10/02-exercicio.php
@@ -11,6 +11,23 @@
     <div>
         <?php
             $d = isset($_GET["ds"])?$_GET["ds"]:0;
+
+            switch ($d) {
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6 :
+                    echo "Levanta e vai estudar!  :)";
+                break;
+                case 7:
+                case 8:
+                    echo "Descanse pequeno gafanhoto"; 
+                break;
+                default :
+                echo "Dia da semana inválido";   
+
+            }
         ?>
         <br>
         <br>
