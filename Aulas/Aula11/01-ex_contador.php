9f226ad303f024cdb29aa1a7dbe08ad318167636
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 09:51:42 +0100

    Aula11 - Estrutura de Repetição While

--- Aulas/Aula11/01-ex_contador.php
+++ Aulas/Aula11/01-ex_contador.php
@@ -10,7 +10,12 @@
 <body>
     <div>
         <?php
-            
+            $c=1;
+
+            while($c<=10){
+                echo $c."<br>";
+                $c++;
+            }
         ?>
         <br>
         <br>
