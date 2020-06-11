c6064ded9047739058d7ebd77d93fe645a992d47
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 10:05:47 +0100

    Aula11 - Estrutura de Repetição While

--- Aulas/Aula11/02-ex_parte1.php
+++ Aulas/Aula11/02-ex_parte1.php
@@ -10,7 +10,13 @@
 <body>
     <div>    
        <form method="GET" action="02-ex_parte2.php">
-            Valor 1: <input type="number" name="v1" max="100" min="0" value="0"><br>
+           <?php
+                $c=1;
+                while($c<=5){        
+                    echo "Valor $c: <input type='number' name='v$c' max='100' min='0' value='0'><br>";
+                    $c++;
+                }
+            ?>
             <input type="submit" class="botao" value="Enviar">
        </form> 
        
