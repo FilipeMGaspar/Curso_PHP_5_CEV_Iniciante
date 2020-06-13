bc6bfca7e068c5376a0547f98d9553dbc414195a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-13 10:21:48 +0100

    Aula13 - Estrutura de Repetição For

--- Aulas/Aula13/02-tabuada.php
+++ Aulas/Aula13/02-tabuada.php
@@ -6,11 +6,21 @@
     , initial-scale=1.0">
     <title>Aula13 - Estrutura de Repetição For</title>
     <link rel="stylesheet" href="_css/estilo.css">
+
+    <style>
+        h1{
+            font-size: 19pt;
+        }
+    </style>
+
 </head>
 <body>
     <div>
         <?php
+            $tabnr = isset($_GET["nrtab"]) ? $_GET["nrtab"] : 1;
             
+            echo "<h1>Tabuada do $tabnr</h1>";
+
         ?>
         <br>
         <br>
