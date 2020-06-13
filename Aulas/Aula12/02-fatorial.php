6013a809643385d2d9d99737c80d68ac69efd3ba
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-13 09:32:41 +0100

    Aula12 - Estrutura de Repetição - Do While

--- Aulas/Aula12/02-fatorial.php
+++ Aulas/Aula12/02-fatorial.php
@@ -12,12 +12,14 @@
         <?php
             $v = isset($_GET["val"]) ? $_GET["val"] : 1;
             echo "<h1>Calculo do Fatorial de $v</h1>";
-
+            $fact = 1;
             $c = $v;
             do{
+                $fact *= $c;
                 echo $c." ";
-                $c--;
+                $c--;                
             }while($c>=1); 
+            echo "<br>Igual a $fact";
 
         ?>
         <br>
