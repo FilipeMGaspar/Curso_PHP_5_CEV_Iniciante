84ea50c9b0ea518196b152c7a6c12117e5fcef20
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-13 10:53:53 +0100

    Aula13 - Estrutura de Repetição For

--- Aulas/Aula13/03-numerosprimos.php
+++ Aulas/Aula13/03-numerosprimos.php
@@ -6,24 +6,27 @@
     , initial-scale=1.0">
     <title>Aula13 - Estrutura de Repetição For</title>
     <link rel="stylesheet" href="_css/estilo.css">
+
+    <?php
+         $nr = isset($_GET["num"]) ? $_GET["num"] : 1;
+         $nrdiv = 0;
+         
+         echo "O Número $nr tem como:<br><br>";
+         echo "O Número $nr tem como:<br><br> ..: Multiplos: ";
+         for($i=1; $i<=$nr; $i++){
+             if (($nr % $i)==0){
+                echo $i." ";
+                $nrdiv ++;
+             } 
+         }
+         echo "<br>";
+    ?>
+
 </head>
 <body>
     <div>
         <!-- Verifica se o numero é primo contagem até os póroprio número-->
-        <?php
-            $nr = isset($_GET["num"]) ? $_GET["num"] : 1;
-            $nrdiv = 0;
-            echo "O Número $nr tem como:<br><br>";
-
-            echo "..: Multiplos: ";
-            for($i=1; $i<=$nr; $i++){
-                if (($nr % $i)==0){
-                   echo $i." ";
-                   $nrdiv ++;
-                } 
-            }
-            echo "<br>";
-
+        <?php        
             echo "<br>..: Número de multiplos: $nrdiv";
             echo "<br>";
             echo "<br>..: Resultado: $nr ";
