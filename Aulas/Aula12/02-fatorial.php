5cfec463ecbb22088f24234b9573f90b8d6f4616
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-13 09:26:00 +0100

    Aula12 - Estrutura de Repetição - Do While

---
+++ Aulas/Aula12/02-fatorial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula12 - Estrutura de Repetição Do While</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            $v = isset($_GET["val"]) ? $_GET["val"] : 1;
+            echo "<h1>Calculo do Fatorial de $v</h1>";
+        ?>
+        <br>
+        <br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
+    </div>
+</body>
+</html>
