e9f28815cdc84f831c1b68056e0d0aff24e5ab09
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-14 09:22:23 +0100

    Aula14 - Rotinas em PHP - Parte 1

--- Aulas/Aula14/01-funcao.php
+++ Aulas/Aula14/01-funcao.php
@@ -12,10 +12,15 @@
         <?php
             function soma ($a, $b){
                 $s = $a + $b;
-                echo "A soma vale $s";
+                echo "<p>A soma vale $s</p>";
             }
 
             soma(3, 4);
+            soma(8, 2);
+            $x= 9;
+            $y = 15;
+
+            soma($x, $y);
         
         ?>
         <!--
