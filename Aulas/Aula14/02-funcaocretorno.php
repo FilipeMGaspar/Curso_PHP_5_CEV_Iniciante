94fe266936bb09b872b64c4565490fe98a835b86
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-14 09:27:49 +0100

    Aula14 - Rotinas em PHP - Parte 1

---
+++ Aulas/Aula14/02-funcaocretorno.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula14 - Rotinas em PHP - Parte 1</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            function soma($a, $b){
+                $s = $a + $b;
+                return $s;
+            }
+
+            $x=3;
+            $y=4;
+            $r = soma($x, $y);
+            echo "A soma entre $x e $y é $r";
+
+        ?>
+        <!--
+        <br>
+        <br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
+        -->
+    </div>
+</body>
+</html>
