144e09181858c12912e9a0dadf06beb9d4f675dd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-15 17:43:27 +0100

    Aula16 - Funções String em PHP - parte 1

--- Aulas/Aula16/04-strwordcount.php
+++ Aulas/Aula16/04-strwordcount.php
@@ -15,7 +15,7 @@
             $cont=str_word_count($frase,0); // o 0 conta o número de palavras
             //$cont=str_word_count($frase,1)// o 1 cria um vetor
             //print_r($cont);
-            //$cont=str_word_count($frase,2); // o 2 
+            //$cont=str_word_count($frase,2); // o 2 cria um vetor, mas as mantém com posição dentro da string
             echo "A frase $frase tem $cont palavras<br/>";
 
             
