e487ef9c1ffb7ccffe0aad3b60eef8cfa71abbaf
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-15 17:29:06 +0100

    Aula16 - Funções String em PHP - parte 1

--- Aulas/Aula16/03-wordwrap.php
+++ Aulas/Aula16/03-wordwrap.php
@@ -12,7 +12,7 @@
         <?php
             $t = "Aqui temos um texto gigante criado pelo PHP e vai mostrar como funciona a função WordWrap";
 
-            $r = wordwrap($t, 20);
+            $r = wordwrap($t, 20, "<br>\n");
 
             echo $t;
         ?>
