71efbf93cd0976ec4221259e7d65817ac5a8781b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-15 17:40:01 +0100

    Aula16 - Funções String em PHP - parte 1

---
+++ Aulas/Aula16/04-strwordcount.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula16 - Funções String em PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            //str_word_count
+            $frase ="Eu vou estudar PHP";
+            $cont=str_word_count($frase,0); // o 0 conta o número de palavras
+            //$cont=str_word_count($frase,1)// o 1 cria um vetor
+            //print_r($cont):
+            //$cont=str_word_count($frase,2); // o 2 
+            echo "A frase $frase tem $cont palavras<br/>";
+
+            
+
+        ?>
+        <!--
+        <br>
+        <br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
+        -->
+    </div>
+</body>
+</html>
