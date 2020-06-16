fc25b22b2318f9bbe34927d4129c4f3858604255
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-16 10:36:10 +0100

    Aula18 - Vetores e Matrizes - Parte 1

---
+++ Aulas/Aula18/02-range.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula18 - Vetores e Matrizes - Parte 1</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <pre>
+            <?php
+                $c = range(5,20,2;);//5 inicio , 20 final, 2 soma
+            ?>
+        </pre>
+        <!--
+        <br>
+        <br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
+        -->
+    </div>
+</body>
+</html>
