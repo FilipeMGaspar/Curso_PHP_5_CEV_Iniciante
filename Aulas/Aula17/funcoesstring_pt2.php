cde30cbfe281d22511f40ac7b0e78932d75045b4
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-16 10:10:42 +0100

    Aula17 - Funções String em PHP - Parte 2

--- Aulas/Aula17/funcoesstring_pt2.php
+++ Aulas/Aula17/funcoesstring_pt2.php
@@ -49,7 +49,7 @@
                 $site="Curso Em video";
                 $sub=substr($site,0,5); //inicia no inicio da string
                 //$sub=substr($site,6);// Inica do fim da string e completa até ao fim
-        echo "$sub<br/>";
+                echo "$sub<br/>";
         ?>
         <!--
         <br>
