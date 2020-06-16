219bd0229db61e805c722bb33c1bc6b8a9b9ce9c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-16 09:55:48 +0100

    Aula17 - Funções String em PHP - Parte 2

---
+++ Aulas/Aula17/01-strtolower.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Aula17 - Funções String em PHP - Parte 2</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+    <div>
+        <?php
+            // funcao_strtolower
+            $nome="Maria Ines";
+            $nome2 = strtolower($nome);
+            echo "O Seu nome em minusculas $nome2<br/>";
+
+        ?>
+        <!--
+        <br>
+        <br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>
+        -->
+    </div>
+</body>
+</html>
