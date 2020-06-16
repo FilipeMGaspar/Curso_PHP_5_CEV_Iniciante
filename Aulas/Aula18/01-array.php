1e2e02ba41be6633e9d4c527b761cf90e3635325
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-16 10:33:30 +0100

    Aula18 - Vetores e Matrizes - Parte 1

--- Aulas/Aula18/01-array.php
+++ Aulas/Aula18/01-array.php
@@ -9,10 +9,12 @@
 </head>
 <body>
     <div>
-        <?php
-               $n = array(3,5,8,2);
-               print_r($n); 
-        ?>
+        <pre>
+            <?php
+                   $n = array(3,5,8,2);
+                   print_r($n); 
+            ?>
+        </pre>
         <!--
         <br>
         <br>
