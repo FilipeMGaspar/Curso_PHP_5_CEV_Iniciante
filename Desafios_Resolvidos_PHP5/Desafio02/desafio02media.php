79721e19c8dbd9ed18d8f9eae11ff5ae829cf591
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-18 10:44:55 +0100

    Desafios02 - Média de 3 números.

--- Desafios_Resolvidos_PHP5/Desafio02/desafio02media.php
+++ Desafios_Resolvidos_PHP5/Desafio02/desafio02media.php
@@ -9,15 +9,20 @@
 <body>
     <div>
         <?php
+            echo "Soma e média dos seguintes valores, ";
             for ($i=1; $i<=3; $i++){
                 $v = "num".$i; // $v = num1; num2 e num3
                 $urlv = $v;// $urlv = num1; num2 e num3
           
                 $$var = isset($_GET[$urlv]) ? $_GET[$urlv] : 0 ;//para o caso do valor não ser definido vai ser 0 o através de variaveis de variaveis a variavel $$var vai receber o valor nas caixas de texto num1, num2 e num3
-                
+                echo  $$var." ;";
                 $soma += $$var; 
             }
+            
+            $media = $soma/3;
+            $media = round($media, 2);
             echo "<br><br>Soma = $soma";
+            echo "<br><br>Média = <span class='foco'>$media</span>";
         ?>  
         <br><br>
         <a href="javascript:history.go(-1)" class="botao">Voltar</a>  
