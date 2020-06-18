f7cf5ba6d59720fed9bb35596497420538b4f3c1
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-18 10:01:59 +0100

    Desafios02 - Média de 3 números.

--- Desafios_Resolvidos_PHP5/Desafio02/desafio02media.php
+++ Desafios_Resolvidos_PHP5/Desafio02/desafio02media.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafios para iniciantes em PHP5.</title>
+    <title>Desafio02 - Média Aritemética!</title>
     <link rel="stylesheet" href="_css/style.css">
 </head>
 <body>
     <div>
-            
+        <?php
+            for ($i=1; $i<=3; $i++){
+                $v = "num".$i;
+                echo "$v ";
+            }
+        ?>  
+        <br><br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>  
     </div>
 </body>
 </html>
