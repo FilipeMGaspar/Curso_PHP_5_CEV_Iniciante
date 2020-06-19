ff7bd010a715c112cdfa7e2c286d88aea8ee059d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-19 08:26:24 +0100

    Desafio03 - Divisivel por 2, 5, 10

--- Desafios_Resolvidos_PHP5/Desafio03/desafio03_div.php
+++ Desafios_Resolvidos_PHP5/Desafio03/desafio03_div.php
@@ -23,7 +23,7 @@
                 $resp = $resp." e pelo 10.";
             }
             if(($v%2 !=0) && ($v%5!=0) && ($v%10!=0)){
-                $resp = " por nenhum dos três.";
+                $resp = " não divide pelo 2, nem pelo 5 e nem pelo 10.";
             }
             echo "É divisivel: $resp";
 
