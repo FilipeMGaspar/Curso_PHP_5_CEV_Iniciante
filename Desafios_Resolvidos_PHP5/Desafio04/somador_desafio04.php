c3d4b40ddb5f22998169a2e230b8c8db25c2bbb2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-20 09:38:43 +0100

    Desafio04 - Somador condicional

--- Desafios_Resolvidos_PHP5/Desafio04/somador_desafio04.php
+++ Desafios_Resolvidos_PHP5/Desafio04/somador_desafio04.php
@@ -12,8 +12,9 @@
             $val1 = isset($_GET["tCxvalor1"]) ? $_GET["tCxvalor1"] : "nd";
             $val2 = isset($_GET["tCxvalor2"]) ? $_GET["tCxvalor2"] : "nd";
 
-            if (($val=="nd") || ($val2=="nd")){
-                echo "Impossivel Realizar a soma!";
+            if (($val=="nd") || ($val2=="nd") || ($val1=="") || ($val2=="")){
+                echo "<h2>Impossivel Realizar a soma!<h2>";
+                echo "<br><h3>Os valores não foram definidos!</h3>";
             }else{
                 echo "<h2>A soma de $val1 com $val2</h2>";
             }    
