b135fae6a11a7dab607e18a94ed4fa543eae153b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-20 09:51:23 +0100

    Desafio04 - Somador condicional

--- Desafios_Resolvidos_PHP5/Desafio04/somador_desafio04.php
+++ Desafios_Resolvidos_PHP5/Desafio04/somador_desafio04.php
@@ -18,7 +18,13 @@
             }else{
                 $soma = $val1 + $val2;
                 echo "<h2>A soma de <span class='foco'>$val1</span> com <span class='foco'>$val2</span> tem o valor de <span class='foco'>$soma</span></h2>";
-                
+                if($soma>20){
+                    echo "<br>O resultado da soma é <span class='foco'>$soma</span> que é maior que 20.<br> Por isso vamos somar 5.";
+                    $soma += 5;
+                    echo "<br><h3>Valor da soma condicional <span class='foco'>$soma</span></h3>";
+
+                    
+                }
             }    
         ?>
 
