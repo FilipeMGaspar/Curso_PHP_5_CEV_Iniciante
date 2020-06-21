4fcf91c67d673572a191a6d2cc77a0ad17659e8b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-21 21:59:35 +0100

    Desafio05 - Maior de Idade mais de 18 anos

--- Desafios_Resolvidos_PHP5/Desafio05/Modelo.php
+++ Desafios_Resolvidos_PHP5/Desafio05/Modelo.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio05 - Maior de idade!</title>
+    <title>Desafios para iniciantes em PHP5.</title>
     <link rel="stylesheet" href="_css/style.css">
 </head>
 <body>
     <div>
             
     </div>
+    <br><br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a> 
 </body>
 </html>
