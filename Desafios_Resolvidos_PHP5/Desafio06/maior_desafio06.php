c5fdd77a06223ca91ff90bf675f178a309fc1a56
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-23 08:27:24 +0100

    Desafio06 - Maior e o menor de 3 valores.

--- Desafios_Resolvidos_PHP5/Desafio06/maior_desafio06.php
+++ Desafios_Resolvidos_PHP5/Desafio06/maior_desafio06.php
@@ -11,6 +11,7 @@
         <?php
             $num1 = isset($_GET["num1"]) ? $_GET["num1"] : "nd"
             $num2 = isset($_GET["num2"]) ? $_GET["num2"] : "nd"
+            $num3 = isset($_GET["num3"]) ? $_GET["num3"] : "nd"
         ?>        
         <br><br>
         <a href="javascript:history.go(-1)" class="botao">Voltar</a>     
