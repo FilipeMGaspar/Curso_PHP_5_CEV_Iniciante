b443f753c5a598ce5b8590af32e2c3f85ee83a64
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-23 08:50:19 +0100

    Desafio07 - Números primos entre 0 e 200!

---
+++ Desafios_Resolvidos_PHP5/Desafio07/modelo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio07 - Números primos entre 0 e 200!</title>
+    <link rel="stylesheet" href="_css/style.css">
+</head>
+<body>
+    <div>
+        
+        
+        <br><br>
+        <a href="javascript:history.go(-1)" class="botao">Voltar</a>     
+    </div>
+    
+</body>
+</html>
