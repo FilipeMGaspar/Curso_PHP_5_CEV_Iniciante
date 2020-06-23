617cf53b4350c07236424b30d648eddee1648b85
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-23 08:31:55 +0100

    Desafio06 - Maior e o menor de 3 valores.

--- Desafios_Resolvidos_PHP5/Desafio06/maior_desafio06.php
+++ Desafios_Resolvidos_PHP5/Desafio06/maior_desafio06.php
@@ -9,9 +9,9 @@
 <body>
     <div>
         <?php
-            $num1 = isset($_GET["num1"]) ? $_GET["num1"] : "nd"
-            $num2 = isset($_GET["num2"]) ? $_GET["num2"] : "nd"
-            $num3 = isset($_GET["num3"]) ? $_GET["num3"] : "nd"
+            $num1 = isset($_GET["num1"]) ? $_GET["num1"] : "nd";
+            $num2 = isset($_GET["num2"]) ? $_GET["num2"] : "nd";
+            $num3 = isset($_GET["num3"]) ? $_GET["num3"] : "nd";
 
             if (($num1=="nd") || ($num2=="nd") || ($num3=="nd")){
                 echo "<h2>Os números a comparar, não foram definidos!</h2>";    
