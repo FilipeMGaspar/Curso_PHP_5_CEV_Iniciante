51a2fe5e1f39bf565093172d4eba7221bb3774d3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-24 17:14:35 +0100

    Desafio08 - Soma dos números pares - Enviados pelo utilizador

--- Desafios_Resolvidos_PHP5/Desafio08/soma_pares_desafio08.php
+++ Desafios_Resolvidos_PHP5/Desafio08/soma_pares_desafio08.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio08 - Soma dos números pare!</title>
+    <title>Desafio08 - Soma dos números pares!</title>
     <link rel="stylesheet" href="_css/style.css">
 </head>
 <body>
     <div>
-        
-        
+        <?php
+            $val = $_GET["tCxVal"];
+            $v[] = $val;
+            
+            print_r($v);
+        ?>        
         <br><br>
         <a href="javascript:history.go(-1)" class="botao">Voltar</a>     
     </div>
