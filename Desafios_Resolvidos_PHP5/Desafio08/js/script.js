1be8fe64fa3ee53c3ef7c95615229a02b6b5c4a5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-24 10:33:10 +0100

    Desafio08 - Soma dos números pares - Enviados pelo utilizador

--- Desafios_Resolvidos_PHP5/Desafio08/js/script.js
+++ Desafios_Resolvidos_PHP5/Desafio08/js/script.js
@@ -10,6 +10,9 @@ function iniciar(){
 function adicionar(){
     if(cxVal.value.length==0){
         alert ('!![ERRO]!! Não foi informado nenhum número!')
+        iniciar()
+    }else{
+        alert ('Tudo ok podemos continuar')
     }
 }
 
