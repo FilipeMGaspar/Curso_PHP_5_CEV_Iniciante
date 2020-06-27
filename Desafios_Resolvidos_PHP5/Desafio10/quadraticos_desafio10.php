c79587c5d0c50db43d8f21c8ab8e204690b50f61
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-27 09:08:23 +0100

    Desafio10 - Quadrado de um número!

--- Desafios_Resolvidos_PHP5/Desafio10/quadraticos_desafio10.php
+++ Desafios_Resolvidos_PHP5/Desafio10/quadraticos_desafio10.php
@@ -8,8 +8,18 @@
 </head>
 <body>
     <div>
-        
-        
+        <?php
+            for($i=1; $i<=5; $i++){
+                $v = "val".$i;
+                $url = $v;
+                $$val = isset($_GET[$url]) ? $_GET[$url] : "nd";
+
+                if ($$val == "nd"){
+                    echo "<h2>Valores indefinidos. Impossivel Continuar</h2>";
+                    $i = 5;
+                }
+            }
+        ?>        
         <br><br>
         <a href="javascript:history.go(-1)" class="botao">Voltar</a>     
     </div>
