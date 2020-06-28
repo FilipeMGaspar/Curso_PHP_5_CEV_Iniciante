e3a1943e42d8cfde77732a1b28491091397be428
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-28 10:46:26 +0100

    Desafio10a - Números exponenciados

---
+++ Desafios_Resolvidos_PHP5/Desafio10a/desafio10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio10_A!</title>
+    <link rel="stylesheet" href="_css/style.css">
+</head>
+<body>
+    <div>
+        <h2>Números Exponenciados!</h2><br>
+         <form method="GET" action="quadraticos_desafio10.php">
+             <?php
+               
+             ?>
+            <input type="submit" class="botao" value="Calcular">
+         </form>   
+    </div>
+</body>
+</html>
