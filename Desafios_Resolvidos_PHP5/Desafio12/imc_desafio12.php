6b0539c486e8bed1ed1c69e70d2091bf50f6a839
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-30 10:29:45 +0100

    Desafio12 - Calculo de IMC

--- Desafios_Resolvidos_PHP5/Desafio12/imc_desafio12.php
+++ Desafios_Resolvidos_PHP5/Desafio12/imc_desafio12.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafios para iniciantes em PHP5!</title>
+    <title>Desafio12 - Calculadora de IMC!</title>
     <link rel="stylesheet" href="_css/style.css">
 </head>
 <body>
     <div>
-        
+        <h2>Calculo do Índice de Massa Corporal - IMC</h2><br>
         
         <br><br>
         <a href="javascript:history.go(-1)" class="botao">Voltar</a>     
