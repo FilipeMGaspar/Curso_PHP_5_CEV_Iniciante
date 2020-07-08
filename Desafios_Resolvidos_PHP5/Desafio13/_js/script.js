b2cff0927fe2488d2a39691e48482ffdb45ebd30
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-07-08 09:15:41 +0100

    Desafio13 - O Maior de 5 Números

--- Desafios_Resolvidos_PHP5/Desafio13/_js/script.js
+++ Desafios_Resolvidos_PHP5/Desafio13/_js/script.js
@@ -20,12 +20,14 @@ function adicionar(){
 function adicionarNaLista(num){    
     let nrElementos = 1
 
-    while (nrElementos<=5){
+    if (nrElementos<=5){
         let itenOption = document.createElement('option')
         itenOption.text = `${num}`
         CxSelNr.appendChild(itenOption)
         nrElementos ++
+    }else{
+        
     }
-    
+
 
 }
